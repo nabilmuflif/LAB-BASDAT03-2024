5ff26ed282a852e7f63b309ff43374d5d399ea49
nabilmuflif <nabilmuflif2505@gmail.com>
2024-09-18 16:14:26 +0800

    praktikum 2 upload

---
+++ H071231036/Praktikum 2/tp2.sql
@@ -0,0 +1,17 @@
+-- SELECT productName, productCode, quantityInStock FROM products
+-- WHERE quantityInStock >= 5000 
+-- AND quantityInStock <=6000
+
+-- SELECT orderNumber, orderDate, STATUS, customerNumber FROM orders
+-- WHERE STATUS <> 'shipped'
+-- ORDER BY customerNumber
+
+-- SELECT employeeNumber, firstname, lastname, email, jobTitle FROM employees
+-- WHERE jobTitle = 'Sales Rep'
+-- LIMIT 10
+
+-- SELECT productName, productCode, productLine, buyprice FROM products
+-- ORDER BY buyprice DESC 
+-- LIMIT 5, 10
+
+-- SELECT DISTINCT country, city FROM customers
